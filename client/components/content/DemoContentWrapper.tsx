19b3f6b537761aa8e06cab76ccb98519499b3ce3
Jack Crawford <primes_unit.05@icloud.com>
2024-06-23 00:14:46 +1200

    demo page wrapper

---
+++ client/components/content/DemoContentWrapper.tsx
@@ -0,0 +1,7 @@
+export default function DemoContent(word: string | number | null) {
+  return (
+    <div>
+      <h1>Hello {word}!</h1>
+    </div>
+  )
+}
--- client/components/pages/Demo.tsx
+++ client/components/pages/Demo.tsx
@@ -1,7 +1,5 @@
+import DemoContent from '../content/DemoContentWrapper'
+
 export default function Demo() {
-  return (
-    <div className="min-h-screen bg-gray-800">
-      <h1 className="p-10 text-4xl text-white">demo</h1>
-    </div>
-  )
+  return <div>{DemoContent('World')}</div>
 }
